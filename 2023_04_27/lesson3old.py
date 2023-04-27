4fea2d5a123cd96ebc0bc59ca5b8054258e9e527
Kochung Lin 林可中 <kl30822.python@gmail.com>
2023-04-27 15:06:00 +0800

    modify

--- 2023_04_27/lesson3old.py
+++ 2023_04_27/lesson3old.py
@@ -10,11 +10,3 @@ def index():
     message += f"<li>user_agent:{request.headers.get('User-Agent')}</li>"
     message += f"<li>Accept-Language:{request.headers.get('Accept-Language')}</li></ul>"
     return message
-
-@app.route("/user/<name>")
-def user(name):
-    return f"<h1>Hello!{name}</h1>"
-
-@app.route("/error")
-def error():
-    return "<h1>error</h1>",404
