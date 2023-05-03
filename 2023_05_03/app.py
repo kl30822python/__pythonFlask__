9028c18ab17724c2d92ec654f20535864e94e57d
Kochung Lin 林可中 <kl30822.python@gmail.com>
2023-05-03 15:01:53 +0800

    modify

--- 2023_05_03/app.py
+++ 2023_05_03/app.py
@@ -3,8 +3,4 @@ from flask import Flask,render_template
 app = Flask(__name__)
 @app.route("/")
 def index():
-    return render_template("index.jinja.html")
-
-@app.route("/learning/")
-def learning():
     return render_template("learning.jinja.html")
